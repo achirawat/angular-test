a984fff1974f54afeb426b551e5628a1f47a1dc4
achirawat <achirawat@angstromsolutions.net>
2017-06-22 15:57:51 +0700

    update3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from '../products/product-list.component';
 import { ProductFilterPipe } from '../products/product-filter.pipe';
 import { StarComponent } from '../shared/star.component';
-import { HttpModule } from '@angular/http';
+
 
 @NgModule({
   declarations: [
--- src/products/product.service.ts
+++ src/products/product.service.ts
@@ -9,10 +9,12 @@ import 'rxjs/add/operator/catch';
 @Injectable()
 
 export class ProductService {
-    private _productUrl = 'http://localhost:4200/src/api/products/products.json';
-    constructor(private _http: Http) {}
+    private productUrl = 'api/products/products.json';
+
+    constructor(private http: Http) {}
+    
     getProducts(): Observable<IProduct[]> {
-        return this._http.get(this._productUrl)
+        return this.http.get(this.productUrl)
             .map((response: Response) => <IProduct[]> response.json())
             .do(data => console.log('All: ' + JSON.stringify(data)))
             .catch(this.handleError);
